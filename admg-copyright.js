a37c4db0506e0243e064ee22bfcbd1d0ce22a4eb
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-14 18:12:49 +0700

    Update admg-copyright.js

--- admg-copyright.js
+++ admg-copyright.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 $(document).ready(function () {
-$('#copyright').html('<a href='https://www.andaiminh.com/' >Hùng Coder Blog</a>');
+$('#copyright').html('<a href='https://www.andaiminh.com/' >AN ĐẠI MINH GROUP</a>');
 setInterval(function () {
 if (!$('#copyright:visible').length) window.location.href = 'https://www.andaiminh.com/'
 }, 1000)
