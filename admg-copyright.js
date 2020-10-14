72c138dc934f8273dc2094856db41a2d5fd8ac9c
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-14 18:11:12 +0700

    Create admg-copyright.js

---
+++ admg-copyright.js
@@ -0,0 +1,8 @@
+//<![CDATA[
+$(document).ready(function () {
+$('#copyright').html('<a href='https://www.andaiminh.com/' >Hùng Coder Blog</a>');
+setInterval(function () {
+if (!$('#copyright:visible').length) window.location.href = 'https://www.andaiminh.com/'
+}, 1000)
+})
+//]]>
