2ae11a681d986c1f5e060e3afe1e0a8866199e09
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-14 18:14:55 +0700

    Create images-lh4.js

---
+++ images-lh4.js
@@ -0,0 +1,6 @@
+//<![CDATA[
+var images = document.getElementsByTagName("img");
+for(var i=0;i<images.length;i++) {
+images[i].src = images[i].src.replace(/[0-9]+.bp.blogspot.com/,"lh4.googleusercontent.com");
+}
+//]]>
