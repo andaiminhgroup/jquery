636cfb69abbbbef7147ac301dfca023e84ba2af2
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-25 13:11:44 +0700

    Create GTM-TNH72BK.js

---
+++ GTM-TNH72BK.js
@@ -0,0 +1,5 @@
+(function(a,s,y,n,c,h,i,d,e){s.className+=&#39; &#39;+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(&#39; ?&#39;+y),&#39;&#39;)};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,&#39;async-hide&#39;,&#39;dataLayer&#39;,4000,
+{&#39;GTM-TNH72BK&#39;:true});
