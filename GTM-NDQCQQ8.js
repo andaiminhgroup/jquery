88953b7d9f57a56450c43f865aca3d11e5439755
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-25 13:10:41 +0700

    Create GTM-NDQCQQ8.js

---
+++ GTM-NDQCQQ8.js
@@ -0,0 +1,5 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({&#39;gtm.start&#39;:
+new Date().getTime(),event:&#39;gtm.js&#39;});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s);j.async=true;j.src=
+&#39;https://www.googletagmanager.com/gtm.js?id=&#39;+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,&#39;script&#39;,&#39;dataLayer&#39;,&#39;GTM-NDQCQQ8&#39;);
