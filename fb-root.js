0ac69addac89760a1041554dd0b5b1b9f18a1ca9
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-25 14:26:15 +0700

    Create fb-root.js

---
+++ fb-root.js
@@ -0,0 +1,7 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = &quot;//connect.facebook.net/vi_VN/sdk.js#xfbml=1&amp;version=v6.0&quot;;
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, &#39;script&#39;, &#39;facebook-jssdk&#39;));
