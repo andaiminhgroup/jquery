c95291818228eabfc3021e4f38b92d4b6e1ab6ce
AN ĐẠI MINH GROUP <72484357+andaiminhgroup@users.noreply.github.com>
2020-10-25 13:14:31 +0700

    Create admg-json.js

---
+++ admg-json.js
@@ -0,0 +1,15 @@
+{
+  &quot;@context&quot; : &quot;http://schema.org&quot;,
+  &quot;@type&quot; : &quot;LocalBusiness&quot;,
+  &quot;name&quot; : &quot;AN ĐẠI MINH&quot;,
+  &quot;image&quot; : &quot;https://andaiminhgroup.github.io/images/logo-home-andaiminh.png&quot;,
+  &quot;telephone&quot; : &quot;0971 949 963&quot;,
+  &quot;email&quot; : &quot;andaiminh.contacts@gmail.com&quot;,
+  &quot;address&quot; : {
+    &quot;@type&quot; : &quot;PostalAddress&quot;,
+    &quot;streetAddress&quot; : &quot;Số 156A An Dương Vương&quot;,
+    &quot;addressCountry&quot; : &quot;, P.16, Q.8, Tp. Hồ Chí Minh, VN&quot;,
+    &quot;postalCode&quot; : &quot;700000&quot;
+  },
+  &quot;url&quot; : &quot;http://www.andaiminh.com/p/smarthome.html&quot;
+}
